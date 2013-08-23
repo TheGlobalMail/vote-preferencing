989857f4552c77f6dff8f1674f6f33a456635edb
B.J. Rossiter <b.j.rossiter@gmail.com>
2013-08-23 14:35:16 +1000

    experiments with secondary force on labels

--- app/js/app.js
+++ app/js/app.js
@@ -1,187 +1,171 @@
 (function() {
   'use strict';
 
-  var width = 960,
-      height = 960;
+	var w = 960, h = 960;
 
-  var json;
+	var labelDistance = 0;
 
-  var svg = d3.select("#visualisation").append("svg")
-      .attr("width", width)
-      .attr("height", height);
+	var vis = d3.select("#visualisation").append("svg:svg").attr("width", w).attr("height", h);
+
+	var nodes = [];
+	var labelAnchors = [];
+	var labelAnchorLinks = [];
+	var links = [];
+
+  var data = {};
 
   var activeLink;
 
-  var force = d3.layout.force()
-      .gravity(0.05)
-      .distance(function(d){
-        var distance = activeLink ? d.individualValue * 9 : d.mutualValue * 5;
-        return distance;
-      })
-      .charge(function(d){
-        return -1 * Math.pow(2, activeLink ? d.individualValue : d.mutualValue);
-      })
-      .size([width, height])
-      .on("tick", tick);
-
-  var link =  svg.selectAll(".link");
-  var node = svg.selectAll(".node");
-
-  var mutualLinkData, linkData, nodeData;
-
-  function tick(){
-
-    var q = d3.geom.quadtree(node),
-      i = 0,
-      n = node.length;
-
-    while (++i < n) {
-      q.visit(collide(node[i]));
-    };
-
-    link.attr("x1", function(d) { return d.source.x;  })
-      .attr("y1", function(d) { return d.source.y;  })
-      .attr("x2", function(d) { return d.target.x;  })
-      .attr("y2", function(d) { return d.target.y;  });
-
-    //node.attr("cx", function(d) { return d.x;  })
-    //  .attr("cy", function(d) { return d.y;  });
-
-    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")";  });
-  }
-
-  function collide(node) {
-    var r,
-    nx1 = node.x - r,
-    nx2 = node.x + r,
-    ny1 = node.y - r,
-    ny2 = node.y + r;
-    node.radius = 5;
-    return function(quad, x1, y1, x2, y2) {
-      if (quad.point && (quad.point !== node)) {
-        var x = node.x - quad.point.x,
-        y = node.y - quad.point.y,
-        l = Math.sqrt(x * x + y * y),
-        r = 50; //node.radius + quad.point.radius;
-        if (l < r) {
-          l = (l - r) / l * .5;
-          node.x -= x *= l;
-          node.y -= y *= l;
-          quad.point.x += x;
-          quad.point.y += y;
-
-
-        }
-        return x1 > nx2
-          || x2 < nx1
-          || y1 > ny2
-          || y2 < ny1;
-      }
-    };
-  }
-
-  d3.json("/data/nsw.json", function(error, data) {
-    json = {};
-    json.nodes = [];
-    json.links = [];
-    json.nodeIndex = {};
+  
+  d3.json("/data/nsw.json", function(error, json) {
+    data.nodes = [];
+    data.labelAnchors = [];
+    data.links = [];
+    data.labelAnchorLinks = [];
+    data.nodeIndex = {};
     // Extract parties
-    _.each(data, function(row, index){
-      json.nodes.push({ name: row.Party });
-      json.nodeIndex[row.Party] = index;
+    _.each(json, function(row, index){
+      var node = {name: row.Party, label: row.Party.replace(/ party.*/i, '') };
+      data.nodes.push(node);
+      data.labelAnchors.push({node: node });
+      data.labelAnchors.push({node: node });
+      data.nodeIndex[row.Party] = index;
     });
     // Build links
-    json.names = _.pluck(json.nodes, 'name');
-    _.each(json.names, function(name, index){
-      var preferences = data[index];
+    data.names = _.pluck(data.nodes, 'name');
+    _.each(data.names, function(name, index){
+      var preferences = json[index];
       delete preferences.Party;
       delete preferences[name];
       _.each(preferences, function(score, target){
-        var scoreF = parseFloat(score)
-        json.links.push({
+        var scoreF = parseFloat(score);
+        data.links.push({
           source: index,
-          target: json.nodeIndex[target],
-          value: scoreF,
+          target: data.nodeIndex[target],
           individualValue: scoreF
         });
       });
     });
     // let's calculate mutual.. TODO could be so much more efficient
-    _.each(json.links, function(link){
-      var coLink = _.detect(json.links, function(l){
+    _.each(data.links, function(link){
+      var coLink = _.detect(data.links, function(l){
         return l.target === link.source &&
-          l.source == link.target;
+          l.source === link.target;
       });
-      link.mutualValue = (link.individualValue + coLink.individualValue) / 2 
+      link.mutualValue = (link.individualValue + coLink.individualValue) / 2 ;
+      link.weight =  link.mutualValue;
+    });
+    _.each(data.nodes, function map(n, index){
+      data.labelAnchorLinks.push({source: index * 2, target: index * 2 + 1, weight: 1 });
     });
     mutual();
   });
-
+  
   function mutual(){
-    nodeData = json.nodes;
-    linkData = json.links;
+    nodes = data.nodes;
+    labelAnchors = data.labelAnchors;
+    links = data.links;
+    labelAnchorLinks = data.labelAnchorLinks;
     update();
   }
 
   function update(){
-    force
-        .nodes(nodeData)
-        .links(linkData)
-        .start();
 
-    link = link.data(activeLink ? linkData : []);
+    var force = d3.layout.force().size([w, h]).nodes(nodes)
+      .links(links)
+      //.gravity(1).linkDistance(50).charge(-3000)
+      .gravity(0.05)
+      .linkDistance(function(d){
+        console.error('using ' + d.mutualValue)
+        var distance = activeLink ? d.individualValue * 9 : d.mutualValue * 5;
+        return distance;
+      })
+      .charge(function(d){
+        console.error('using: ' + d.mutualValue)
+        console.error(d)
+        return -1 * Math.pow(2, activeLink ? d.individualValue : d.mutualValue);
+      });
+      /*
+      .linkStrength(function(x) {
+        return x.weight * 10;
+      });
+      */
 
-    // remove anything not in the data anymore
-    link.exit().remove();
 
-    // Add a line
-    link.enter().insert("line", ".node")
-          .attr("class", "link");
+    force.start();
 
-    node = node.data(nodeData);
+		var force2 = d3.layout.force().nodes(labelAnchors).links(labelAnchorLinks).gravity(0).linkDistance(10).linkStrength(8).charge(-100).size([w, h]);
+    force2.start();
 
-    node.exit().remove();
+    //var link = vis.selectAll("line.link").data(links).enter().append("svg:line").attr("class", "link").style("stroke", "#CCC");
 
-    var nodeEnter = node.enter()
-    
-    var inside = nodeEnter.append("g")
-      .attr("class", "node")
-      .attr("pointer-events", "all")
-      .on("click", click)
-      .call(force.drag);
+    var node = vis.selectAll("g.node").data(force.nodes()).enter().append("svg:g").attr("class", "node");
+    node.append("svg:circle").attr("r", 5).style("fill", "#555").style("stroke", "#FFF").style("stroke-width", 3);
+    node.call(force.drag);
 
-    inside.append("svg:circle")
-      .attr("r", 10)
-      .style("fill", "#555")
-      .style("stroke", "#FFF")
-      .style("stroke-width", 3);
 
-    inside.append("text")
-        .attr('dx', '10')
-        .attr('text-anchor', 'start')
-        .attr("dy", ".35em")
-        .attr("pointer-events", "none")
-        .text(function(d) { return d.name.replace(/ Party.*/, '')});
-  }
+    var anchorLink = vis.selectAll("line.anchorLink").data(labelAnchorLinks)//.enter().append("svg:line").attr("class", "anchorLink").style("stroke", "#999");
 
-  function click(d){
-    if (d3.event.defaultPrevented) return; // ignore drag
-    if (activeLink === d.index){
-      activeLink = null;
-      return mutual();
+    var anchorNode = vis.selectAll("g.anchorNode").data(force2.nodes()).enter().append("svg:g").attr("class", "anchorNode");
+    anchorNode.append("svg:circle").attr("r", 0).style("fill", "#FFF");
+      anchorNode.append("svg:text").text(function(d, i) {
+      return i % 2 == 0 ? "" : d.node.label
+    }).style("fill", "#555").style("font-family", "Arial").style("font-size", 12);
+
+    var updateLink = function() {
+      this.attr("x1", function(d) {
+        return d.source.x;
+      }).attr("y1", function(d) {
+        return d.source.y;
+      }).attr("x2", function(d) {
+        return d.target.x;
+      }).attr("y2", function(d) {
+        return d.target.y;
+      });
+
     }
-    activeLink = d.index;
-    // rearrange links to be alphabetical distance
-    var partyLinks = [];
-    json.links.forEach(function(link){
-      var targetName;
-      if (link.source.index === d.index){
-        partyLinks.push(link);
-      }
-    });
 
-    linkData = partyLinks;
-    update();
+    var updateNode = function() {
+      this.attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      });
+
+    }
+
+
+    force.on("tick", function() {
+
+      force2.start();
+
+      node.call(updateNode);
+
+      anchorNode.each(function(d, i) {
+        if(i % 2 == 0) {
+          d.x = d.node.x;
+          d.y = d.node.y;
+        } else {
+          var b = this.childNodes[1].getBBox();
+
+          var diffX = d.x - d.node.x;
+          var diffY = d.y - d.node.y;
+
+          var dist = Math.sqrt(diffX * diffX + diffY * diffY);
+
+          var shiftX = b.width * (diffX - dist) / (dist * 2);
+          shiftX = Math.max(-b.width, Math.min(0, shiftX));
+          var shiftY = 5;
+          this.childNodes[1].setAttribute("transform", "translate(" + shiftX + "," + shiftY + ")");
+        }
+      });
+
+
+      anchorNode.call(updateNode);
+
+      //link.call(updateLink);
+      anchorLink.call(updateLink);
+
+    });
   }
+
 
 }());
